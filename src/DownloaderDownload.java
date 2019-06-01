88bb01432c0c0f776ea713810153663a4b702068
Ido <idomarko1998@gmail.com>
2019-06-02 00:21:56 +0300

    holdmovie in on

--- src/DownloaderDownload.java
+++ src/DownloaderDownload.java
@@ -20,7 +20,6 @@ public class DownloaderDownload extends Downloader {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    System.out.println(context.downloadSpeed);
                     context.percent += context.downloadSpeed;
                     //System.out.println(context.percent);
                 }
